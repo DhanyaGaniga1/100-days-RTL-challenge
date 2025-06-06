77104718e67100c7a09e4254c9b8cfbb068e4088
DhanyaGaniga1 <dhanyajganiga2004@gmail.com>
2025-06-06 19:54:55 +0530

    Add files via upload

---
+++ Military_time_counter_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+module MILITARY_TIME_COUNTER_TB();
+reg CLK;
+reg RESET;
+wire [4:0]Q;
+MILITARY_TIME_COUNTER DM (.CLK(CLK), .RESET(RESET), .Q(Q));
+
+initial 
+begin
+
+CLK=1'b0;
+forever #5 CLK=~CLK;
+end
+
+initial 
+begin
+RESET=1;
+#10 RESET=0;
+#95;
+
+RESET=1;
+#10 RESET=0;
+#335
+
+$finish;
+end
+endmodule
