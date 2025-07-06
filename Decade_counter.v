cdd39be54da5e6ab05d72f1c3de136d7230e80f2
DhanyaGaniga1 <dhanyajganiga2004@gmail.com>
2025-07-06 11:21:55 +0530

    Add files via upload

---
+++ Decade_counter.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module Decade_counter(clk,rst,q);
+input clk,rst;
+output reg [3:0]q;
+
+always@(posedge clk or posedge rst)
+begin
+if(rst)
+q<=4'b0000;
+else
+if (q==4'b1001)
+q<=4'b0000;
+else
+q<=q+1;
+end
+endmodule
