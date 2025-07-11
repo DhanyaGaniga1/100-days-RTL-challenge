7a986c49b4b4bf8d6ab0c5ed0607db64d704e4df
DhanyaGaniga1 <dhanyajganiga2004@gmail.com>
2025-07-11 10:33:15 +0530

    Add files via upload

---
+++ Moore_state_machine_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+
+module Moore_state_machine_tb();
+reg x,clk;
+wire y;
+Moore_state_machine dut(.x(x), .clk(clk), .y(y));
+
+initial 
+begin
+clk=1'b0;
+forever #5 clk=~clk;
+end
+
+initial 
+begin
+x=1; #10
+x=1; #10 
+x=0; #10 
+x=1; #10 
+x=0; #10 
+x=1; #10 
+x=0; #10 
+x=1; #10 
+x=0; #10 
+x=1; #10 
+x=1; #10 ;
+$stop;
+end
+endmodule
+
